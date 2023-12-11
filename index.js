002df6cfed8371fbadcb8c57cc60e1f79f43bd0d
TimothyMcCann <timothysmccann@gmail.com>
2023-12-11 08:13:00 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+console.log(multiply);
+
+const random = Math.floor(Math.random()) + 1
+console.log(random)
+
+const num3 = 18
+const num4 = 7
+const mod = (num3 % num4)
+console.log(mod)
+
+const max = Math.max(5, 10, 15, 20)
